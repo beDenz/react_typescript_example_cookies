6203c29034687139f5d194acc573f9f1f413e860
Denis Barvitskiy <bedenz@gmail.com>
2021-04-02 10:58:41 +0700

    #0000 feat: init project

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
+  // stories: [
+  //   '../src/components/**/*.stories.@(ts|tsx)',
+  //   '../src/stories/**/*.stories.mdx',
+  //   '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
+  // ],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+}
