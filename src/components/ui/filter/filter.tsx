cffbd12a07ab2dd31f7e285e5062a997be44a327
Denis Barvitskiy <bedenz@gmail.com>
2021-04-03 16:51:45 +0700

    #0000 fix: filter fix

--- src/components/ui/filter/filter.tsx
+++ src/components/ui/filter/filter.tsx
@@ -1,19 +1,29 @@
-import styles from './filter.module.scss'
+// import styles from './filter.module.scss'
 import FilterListIcon from '@material-ui/icons/FilterList'
 import { ButtonBase } from '@material-ui/core'
+
+import { makeStyles } from '@material-ui/core/styles'
 
 export type FilterProps = {
   onClick: () => void
 }
 
+const useStyles = makeStyles({
+  root: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    border: 1,
+    borderStyle: 'solid',
+    borderColor: '#dddddd',
+  },
+})
+
 const Filter: React.FC<FilterProps> = ({ onClick }) => {
+  const classes = useStyles()
+
   return (
-    <ButtonBase
-      classes={{
-        root: styles.root,
-      }}
-      onClick={onClick}
-    >
+    <ButtonBase classes={classes} onClick={onClick}>
       <FilterListIcon />
     </ButtonBase>
   )
